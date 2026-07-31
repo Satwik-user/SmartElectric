6e6aa04daec778137f22b0b0cb19faf32646f2da
Satwik-user <satwik2400@gamil.com>
2026-07-31 08:37:24 +0000

    Fix NGINX api proxy routing and frontend API port for remote devices

--- edge/scripts/7-setup-nginx.sh
+++ edge/scripts/7-setup-nginx.sh
@@ -33,7 +33,7 @@ server {
 
     # Route /api to the FastAPI backend running on localhost:8000
     location /api/ {
-        proxy_pass http://127.0.0.1:8000/;
+        proxy_pass http://127.0.0.1:8000;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
